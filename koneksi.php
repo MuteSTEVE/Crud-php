ecafa674e3ceebdd60c875672e8b0adc32c5842b
MuteSTEVE <Dr.MuteSTEVE2@protonmailcom>
2023-08-27 08:29:01 +0700

    Adding bootstrap and fixing some script placement

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,21 @@
+<?php
+  $host = 'localhost';
+  $dbname = 'operator';
+  $user = 'postgres';
+  $password = 'postgres';
+
+  $connection = pg_connect("host=$host dbname=$dbname user=$user password=$password");
+  $status = ($connection) ? "<h1>Koneksi terhubung</h1>" : "<h1>Gagal</h1>";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <!-- bootstrap -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <script src="js/bootstrap.bundle.min.js"></script>
+
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
@@ -9,18 +23,7 @@
   <p>Koneksi.php</p>    
   <hr>
   <?php
-    $host = 'localhost';
-    $dbname = 'operator';
-    $user = 'postgres';
-    $password = 'postgres';
-
-    $connection = pg_connect("host=$host dbname=$dbname user=$user password=$password");
-    if ($connection) {
-      echo "<h1>Koneksi Terhubung</h1>";
-    }
-    else {
-      echo "<h1>Gagal</h1>";
-    }
+    echo $status;
   ?>
 </body>
 </html>
