59f1785d6b880cfef24374bcd6d59acfae77e8f7
MuteSTEVE <Dr.MuteSTEVE2@protonmailcom>
2023-08-31 10:54:45 +0700

    Memindahkan css dan js ke bootstrap/

--- koneksi.php
+++ koneksi.php
@@ -13,8 +13,8 @@
   <head>
     <meta charset="UTF-8">
     <!-- bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <script src="js/bootstrap.bundle.min.js"></script>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <link rel="stylesheet" href="fontawesome/css/fontawesome.min.css">
 
